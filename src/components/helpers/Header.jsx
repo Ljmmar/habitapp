9e0cb4a8a5322dc2414fd57a32bf9f134c0f66ab
Ljmmar <martinezrljm@gmail.comi>
2023-09-29 20:24:12 -0500

    se agrega opcion de search al header

--- src/components/helpers/Header.jsx
+++ src/components/helpers/Header.jsx
@@ -6,14 +6,18 @@ const Header = () => {
             <h1 >Habitapp</h1>
 
             <div className="logo">
-            <Link to={'/'}> <img src='./public/icons/casa.png' alt="Logo" /></Link>
-            
+                <Link to={'/'}> <img src='./public/icons/casa.png' alt="Logo" /></Link>
+
 
             </div>
             <div className='options'>
                 <Link to={'/contact'}>Contact</Link>
                 <Link to={'/projects'}>Projects</Link>
                 <Link to={'/onsale'}>On sale</Link>
+            </div>
+            <div className="searchh">
+                <input type="text"  placeholder="Search"/>
+                <span class="icono">&#128269;</span>
             </div>
         </header>
     )
