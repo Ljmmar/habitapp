88389bd6d9c220ad39a3d9ea43e8ed0173139764
Ljmmar <martinezrljm@gmail.comi>
2023-11-26 09:37:33 -0500

    login update

--- src/components/layouts/Login.jsx
+++ src/components/layouts/Login.jsx
@@ -1,40 +1,37 @@
 import { useState } from 'react';
-// import { useHistory } from 'react-router-dom';
-
+import {  useNavigate } from 'react-router-dom';
 
 const Login = () => {
-
-  // const history = useHistory();
-
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
 
-  const handleLogin = () => {
-
-    e.preventDefault(); 
+  const handleLogin = (e) => {
+    e.preventDefault();
     if (username === 'habitapp' && password === '12345') {
-      history.push('/admin');
-  
+      redircttoadmin()
     } else {
       alert('Nombre de usuario o contraseña incorrectos');
     }
   };
 
+  const redircttoadmin = () => {
+    navigate('/admin');
+  }
 
   return (
     <section className="admin-container">
       <div className="login-container">
         <h1>Login</h1>
         <form action="#" method='post'>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username'/>
-          <input type='password' value={password} onChange={(e)=> setPassword(e.target.value)} placeholder='contraseña' />
-          <button onClick={handleLogin}>Iniciar sesión</button>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
+          <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='contraseña' />
+          <button onClick={handleLogin}>Iniciar sesión</button >
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-
-export default Login
+export default Login;
